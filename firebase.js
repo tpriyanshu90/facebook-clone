3dd773f944fe788a2c9f8cffefd62224fa3e7a63
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2021-07-01 16:57:09 +0530

    added storage container name

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: "facebook-clone-tp9",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  storageBucket: "facebook-clone-tp9.appspot.com",
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APPID
 };
