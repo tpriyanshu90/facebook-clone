31b4dcba747c4583a1e3226f10fadc7698716940
Divyanshu Tiwari <tdivyanshu99@gmail.com>
2023-06-03 19:42:03 +0530

    Added like feature

--- components/Posts.js
+++ components/Posts.js
@@ -12,23 +12,27 @@ function Posts({ posts }) {
         ? realtimePosts?.docs.map((post) => (
             <Post
               key={post.id}
+              postId={post.id}
               name={post.data().name}
               message={post.data().message}
               email={post.data().email}
               timestamp={post.data().timestamp}
               image={post.data().image}
               postImage={post.data().postImage}
+              likes={post.data().likes}
             />
           ))
         : posts.map((post) => {
             <Post
               key={post.id}
+              postId={post.id}
               name={post.name}
               message={post.message}
               email={post.email}
               timestamp={post.timestamp}
               image={post.image}
               postImage={post.postImage}
+              likes={post.likes}
             />;
           })}
     </div>
